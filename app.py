4f23facae005bee30d1c52b4ad737a37dde7153b
Inweregbu Joseph Chukwuka <inwerejosic@protonmail.com>
2021-06-19 11:53:56 +0100

    Today I am making more progress on my flask project

--- app.py
+++ app.py
@@ -1,8 +1,28 @@
+#  Flask imports
+
 from flask import Flask
+from datetime import datetime
+import re
+
 app = Flask(__name__)
 
 #routes
 
 @app.route("/")
 def home():
-    return "Hello Flashy!"
+    return "Hello Flashy!"    
+
+
+@app.route("/hello/<name>")
+def hello_there(name):
+    now = datetime.now()
+    formatted_now = now.strftime("%A, %d, %B, %Y, at %X")
+
+    match_object = re.match("[a-zA-Z]+", name)
+
+    if match_object:
+        clean_name = match_object.group(0)
+    else: 
+        clean_name = "Friend"
+    content = "Hello there, " + clean_name + "! It's " + formatted_now
+    return content 
